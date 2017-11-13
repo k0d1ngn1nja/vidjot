d7905e01a9e8f1d88f6e5725c830e4405e771fac
C0D1NGD0J0 <web-dev@United4Life.local>
2017-11-13 18:44:30 +0100

    basic config updated

--- app/db/index.js
+++ app/db/index.js
@@ -6,13 +6,11 @@ const isProduction =(process.env.NODE_ENV === 'production');
 mongoose.Promise = global.Promise;
 
 if(isProduction){
-	mongoose.connect(config.productionEnv.db, function(err) {		
-  	if (err) console.log(err);
-  	console.log("Connected to remote database");
-	});
+	mongoose.connect(config.productionEnv.db, {useMongoClient: true })
+		.then(() => console.log('Connected to local database'))
+		.catch((err) => console.log("Mongoose connection error occurred ", err));
 } else {
-	mongoose.connect(config.localEnv.db, (err) => {
-		if(err) console.log("Mongoose connection error occurred ", err);
-		console.log('Connected to local database');
-	});
+	mongoose.connect(config.localEnv.db, {useMongoClient: true })
+		.then(() => console.log('Connected to local database'))
+		.catch((err) => console.log("Mongoose connection error occurred ", err));
 }
